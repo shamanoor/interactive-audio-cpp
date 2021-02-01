36623622a3535902223f91181e9f7e2143ff6d34
shamanoor <53785937+shamanoor@users.noreply.github.com>
2021-02-01 23:00:46 +0100

    add most basic file

---
+++ main.cpp
@@ -0,0 +1,11 @@
+/*
+	February project: upgrade interactive audio prototype.
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+	// Use OpenCV to do webcam loading + motion tracking
+
+	return 0;
+}
