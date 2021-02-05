4daf17484a47b03bb70820c5bb27ec53d865f68f
shamanoor <53785937+shamanoor@users.noreply.github.com>
2021-02-05 23:09:08 +0100

    add background subtraction for motion detection
    Shows visual output of motion detected

--- main.cpp
+++ main.cpp
@@ -5,14 +5,17 @@
 #include <opencv2/core.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/videoio.hpp>
+#include <opencv2/video/background_segm.hpp>
+
 
 using namespace std;
 using namespace cv;
 
 int main() {
 	// Use OpenCV to do webcam loading + motion tracking
-	Mat frame;
+	Mat frame, fgMask; // fgMask is for background subtraction
 	VideoCapture cap;
+	Ptr<BackgroundSubtractor> pBackSub = createBackgroundSubtractorKNN(10); // for background subtraction
 
 	int deviceID = 0;
 	int apiID = CAP_ANY;
@@ -24,15 +27,18 @@ int main() {
 		return -1;
 	}
 
-
 	for (;;) {
 		cap.read(frame);
+
+		// update background model
+		pBackSub->apply(frame, fgMask);
 
 		if (frame.empty()) {
 			cerr << "Error: blank frame grabbed.\n";
 		}
 
 		imshow("Live", frame);
+		imshow("FG Mask", fgMask);
 
 		if (waitKey(5) >= 0) {
 			break;
@@ -40,6 +46,5 @@ int main() {
 	}
 
 	waitKey(0);
- 
 	return 0;
 }
