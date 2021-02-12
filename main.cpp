211eb61d78af5dbda0300c90aeb1a7eaeadf851c
shamanoor <53785937+shamanoor@users.noreply.github.com>
2021-02-12 22:55:06 +0100

    use FMOD for audio

--- main.cpp
+++ main.cpp
@@ -9,22 +9,50 @@
 #include <windows.h> // for audio
 #include <codecvt>
 
+#include <fmod.hpp>
 
+using namespace FMOD;
 using namespace std;
 using namespace cv;
 
+FMOD::Sound* song;
+
+
 int main() {
+	FMOD_RESULT result;
+	FMOD::System* syst = NULL;
+
+	result = FMOD::System_Create(&syst);      // Create the main system object.
+	if (result != FMOD_OK)
+	{
+		printf("FMOD error!");
+		exit(-1);
+	}
+
+	result = syst->init(512, FMOD_INIT_NORMAL, 0);    // Initialize FMOD.
+	if (result != FMOD_OK)
+	{
+		printf("FMOD error!\n");
+		exit(-1);
+	}
+
+	FMOD::Sound* song;
+	FMOD::Channel* channel;
+	syst->createSound("vald-rappel.wav", FMOD_DEFAULT, NULL, &song);
+
+	syst->playSound(song, NULL, false, &channel);
+
+
 	// Use OpenCV to do webcam loading + motion tracking
 	Mat frame, fgMask, grayFrame; // fgMask is for background subtraction
 	VideoCapture cap;
 	Ptr<BackgroundSubtractor> pBackSub = createBackgroundSubtractorKNN(10); // for background subtraction
-
 	Scalar avgMotion; // to store the mean value of the motion
+	
 	int deviceID = 0;
 	int apiID = CAP_ANY;
-
-	mciSendString(L"open \"vald-rappel.wav\" type mpegvideo alias wav", NULL, 0, NULL);
-	mciSendString(L"play wav", NULL, 0, NULL);
+	float frequency = 0;
+	channel->getFrequency(&frequency);
 
 	cap.open(deviceID, apiID);
 
@@ -33,6 +61,9 @@ int main() {
 		return -1;
 	}
 	for (;;) {
+		frequency = max(2000, frequency - 100); // constrain frequency
+		channel->setFrequency(frequency);
+
 		cap.read(frame);
 
 		// update background model
@@ -48,25 +79,9 @@ int main() {
 		// to get a quantitative value, we compute the average pixel brightness in the fgMask
 		// closer to 0 means no motion, the higher the number, the more motion there is
 		avgMotion = mean(fgMask);
-		int speed = avgMotion[0];
-		cout << "Average amount of motion: " << avgMotion[0] << endl;
+		int avgMotion_int = avgMotion[0];
+		// cout << "Average amount of motion: " << avgMotion[0] << endl;
 
-		string param_value = "0";
-		if (speed < 5) {
-			param_value = "500";
-		}
-		else {
-			param_value = "1000";
-		}
-
-		string param = "set wav speed ";
-		param = param + param_value;
-
-		// create the parameter to set the speed
-		std::wstring str_turned_to_wstr = std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes(param);
-		LPCWSTR parameter = str_turned_to_wstr.c_str();
-	;
-		mciSendString(parameter, 0, 0, 0);
 
 		if (waitKey(5) >= 0) {
 			break;
